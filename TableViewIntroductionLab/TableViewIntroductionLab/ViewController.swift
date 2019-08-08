149d79a565a2e9f93f3bdd0f41d6cdf62ac84281
Mr Wonderful <mrwonderful@Mrs-MacBook-Pro.local>
2019-08-08 16:27:35 -0400

    completes question one

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -8,13 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var task = Task.allTasks
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+     return task.count
+    }
+    
+
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let table = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "myIdentifier")
+
+        table.textLabel?.text = task[indexPath.row].name
+        table.detailTextLabel?.text = "\(task[indexPath.row].dueDate)"
+        return table
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var tableView: UITableView!
+    
 }
 
